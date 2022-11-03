4a5cbec23615a8d22846aeb9d0d4f8bda8805c7d
LuisTomasini <100858288+LuisTomasini@users.noreply.github.com>
2022-11-03 16:42:49 -0300

    ...

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,11 @@
 import { NativeBaseProvider, StatusBar } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
-import { SignIn } from './src/screens/SignIn';
-import { Loading } from './src/components/Loading';
+import { AuthContextProvider } from './src/contexts/AuthContext';
+
+import { SingIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading'
+
 
 import { THEME } from './src/styles/theme'
 
@@ -11,12 +14,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
+      <AuthContextProvider>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-        { fontsLoaded ? <SignIn /> : <Loading /> }
+        { fontsLoaded ? <SingIn /> : <Loading /> }
+        </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
